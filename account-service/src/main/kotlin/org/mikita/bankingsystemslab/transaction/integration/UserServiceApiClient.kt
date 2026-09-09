a6953850b77e50c4fdc0eb7175471c3517a8e29f
mikita-zadarozhny <nikita.zadorozhny@icloud.com>
2026-09-10 02:57:54 +0400

    Service account-service was integrated with user-service.

---
+++ account-service/src/main/kotlin/org/mikita/bankingsystemslab/transaction/integration/UserServiceApiClient.kt
@@ -0,0 +1,18 @@
+package org.mikita.bankingsystemslab.transaction.integration
+
+import org.mikita.bankingsystemslab.transaction.domain.user.User
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestClient
+
+@Component
+class UserServiceApiClient (
+    private val restClient: RestClient
+) {
+
+    fun getUser(id: Long): User {
+        return restClient.get()
+            .uri("/api/v1/users/{userId}", id)
+            .retrieve()
+            .body(User::class.java)!!
+    }
+}
